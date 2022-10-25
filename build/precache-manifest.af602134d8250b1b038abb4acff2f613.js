4da03b8fa50a66c169a1f915ff8bb971490b18e5
Shaikh-Umar-Farooq <smuf7080@gmail.com>
2022-10-25 20:23:34 +0530

    did

---
+++ build/precache-manifest.af602134d8250b1b038abb4acff2f613.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c45d0f846e9d2fea2f4e8de69657281",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7aaedb2444c065617550",
+    "url": "/static/css/main.0b297ea1.chunk.css"
+  },
+  {
+    "revision": "886855c3bc708f557dd8",
+    "url": "/static/js/2.c739739d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.c739739d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aaedb2444c065617550",
+    "url": "/static/js/main.28eb54a6.chunk.js"
+  },
+  {
+    "revision": "4263751f28d88807f26e",
+    "url": "/static/js/runtime-main.32033953.js"
+  }
+]);
